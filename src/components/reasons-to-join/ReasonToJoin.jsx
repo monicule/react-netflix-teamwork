9f71277dbc9e27c997fab37e5839d14ee34acd7f
Monika <laiskutis.monikai@gmail.com>
2024-08-23 12:21:21 +0300

    img

--- src/components/reasons-to-join/ReasonToJoin.jsx
+++ src/components/reasons-to-join/ReasonToJoin.jsx
@@ -2,10 +2,12 @@ import style from './ReasonsToJoin.module.css';
 
 export function Reason(params) {
     return (
+    
         <div className={style.reason}>
             <h2 className={style.title}>{params.title}</h2>
             <p className={style.description}>{params.description}</p>
-            <div className={style.icon}>{params.icon}</div>
+            <img className={style.icon}/>{params.icon}
         </div>
+
     );
 }
--- src/components/reasons-to-join/ReasonsToJoin.jsx
+++ src/components/reasons-to-join/ReasonsToJoin.jsx
@@ -1,20 +1,25 @@
 import { Reason } from "./ReasonToJoin";
 import style from './ReasonsToJoin.module.css';
+import one from './img/one.webp';
+import two from './img/two.webp';
+import three from './img/three.webp';
+import four from './img/four.webp';
 
 export function Reasons() {
     return (
         <section className={style.reasonsList}>
             <Reason title="Enjoy on your TV"
-            description="Watch on Smart TVs, Playstation, Xbox, Chromecast, Apple TV, Blu-ray players, and more." />
+            description="Watch on Smart TVs, Playstation, Xbox, Chromecast, Apple TV, Blu-ray players, and more." 
+            icon={one} />
             <Reason title="Download your shows to watch offline"
-            description="Save your favorites easily and always have something to watch." />
+            description="Save your favorites easily and always have something to watch."
+            icon={two} />
             <Reason title="Watch everywhere"
-            description="Stream unlimited movies and TV shows on your phone, tablet, laptop, and TV." />
+            description="Stream unlimited movies and TV shows on your phone, tablet, laptop, and TV."
+            icon={three} />
             <Reason title="Create profiles for kids"
-            description="Send kids on adventures with their favorite characters in a space made just for them — free with your membership." />
-
-
-
+            description="Send kids on adventures with their favorite characters in a space made just for them — free with your membership."
+            icon={four} />
         </section>
     );
 }
